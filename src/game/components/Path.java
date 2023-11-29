55dcc4e00428df6abc59b16834df825ae63a3580
Fadta <fvpan01@gmail.com>
2023-11-29 04:22:28 -0300

    added comments to The Great Wall of Code
    Path.java should now be more comprehensible on its logic

--- src/game/components/Path.java
+++ src/game/components/Path.java
@@ -16,42 +16,64 @@ public class Path {
     }
 
     public boolean haveMonster(Long playerId) {
-        return pathBoxes.stream().anyMatch( pathBox -> pathBox.getMonster() != null && pathBox.getMonster().getPlayer().getId().equals(playerId) );
+    	// match criteria: Monster exists && Monster is owned by player($playerID)
+        return pathBoxes.stream().anyMatch(
+        			pathBox ->
+        			pathBox.getMonster() != null && pathBox.getMonster().getPlayer().getId().equals(playerId)
+        		);
     }
 
     public void nextRound(Long playerId, Castle castle) {
-        Optional<PathBox> occupiedPathBox = pathBoxes.stream().filter(pathBox -> pathBox.getMonster() != null && pathBox.getMonster().getPlayer().getId().equals(playerId)).findFirst();
+        Optional<PathBox> occupiedPathBox =
+        		pathBoxes.stream().filter(
+        				pathBox -> // filter: Monster exists & Monster is owned by player($playerID)
+        				pathBox.getMonster() != null && pathBox.getMonster().getPlayer().getId().equals(playerId)
+        		).findFirst();
+        
         if(occupiedPathBox.isPresent()) {
+        	// if PathBox is occupied
             if(playerId.equals(1L)) {
+            	// working with player 1
                 if(occupiedPathBox.get().getNorthBox() == null) {
+                	// north box not occupied (arrived @Castle) => damage player & dispose monster
                     castle.setLife(castle.getLife() - 1);
                     occupiedPathBox.get().setMonster(null);
                 } else {
+                	// there exists north box (not arrived @Castle)
                     if(occupiedPathBox.get().getNorthBox().getMonster() == null) {
+                    	// there is no monster, advance
                         occupiedPathBox.get().getMonster().move(occupiedPathBox.get(), occupiedPathBox.get().getNorthBox());
                     } else {
+                    	// there is a monster => battle
                         System.out.println("Casilla " + occupiedPathBox.get().getNorthBox().getName() + " Esta ocupada!!!");
                         System.out.println("Monstruo " + occupiedPathBox.get().getMonster() + " Ataca a " + occupiedPathBox.get().getNorthBox().getMonster());
                         occupiedPathBox.get().getMonster().attack(occupiedPathBox.get().getNorthBox().getMonster());
                         occupiedPathBox.get().getNorthBox().getMonster().attack(occupiedPathBox.get().getMonster());
                         if(occupiedPathBox.get().getNorthBox().getMonster().getLife() <= 0) {
+                        	// enemy died
                             occupiedPathBox.get().getNorthBox().setMonster(null);
                         }
                     }
                 }
             } else {
+            	// working with player 2
                 if(occupiedPathBox.get().getSouthBox() == null) {
+                	// south box not occupied (arrived @Castle) => damage player & dispose monster
                     castle.setLife(castle.getLife() - 1);
                     occupiedPathBox.get().setMonster(null);
                 } else {
+                	// there exists south box (not arrived @Castle)
                     if (occupiedPathBox.get().getSouthBox().getMonster() == null) {
+                    	// there is no monster, advance
                         occupiedPathBox.get().getMonster().move(occupiedPathBox.get(), occupiedPathBox.get().getSouthBox());
                     } else {
+                    	// there is a monster => battle
                         System.out.println("Casilla " + occupiedPathBox.get().getSouthBox().getName() + " Esta ocupada!!!");
                         System.out.println("Monstruo " + occupiedPathBox.get().getMonster() + " Ataca a " + occupiedPathBox.get().getSouthBox().getMonster());
                         occupiedPathBox.get().getMonster().attack(occupiedPathBox.get().getSouthBox().getMonster());
                         occupiedPathBox.get().getSouthBox().getMonster().attack(occupiedPathBox.get().getMonster());
                         if (occupiedPathBox.get().getSouthBox().getMonster().getLife() <= 0) {
+                        	// enemy died
                             occupiedPathBox.get().getSouthBox().setMonster(null);
                         }
                     }
